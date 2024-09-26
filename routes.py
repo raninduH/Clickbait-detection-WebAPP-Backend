b7398cd18d1bad39246fab9bd3432df908a16d32
Ranindu <raninduharischandra12@gmail.com>
2024-09-26 10:21:15 +0530

    tokenizer issue fixed in supervised

--- routes.py
+++ routes.py
@@ -1,51 +1,20 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
 from models import Title_with_content_batch ,Titles
-import numpy as np
-from tensorflow.keras.preprocessing.text import tokenizer_from_json
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-import tensorflow as tf
+from supervised.title_prediction import predcit_titles
+
 
 router = APIRouter()
 
 
-# Load the supervised model (either .h5 or .keras)
-supervised_model = tf.keras.models.load_model('supervised_clickbait_detection_model_V2.h5')
-
 # POST route to receive an array of titles
 @router.post("/single-title-detection")
-async def process_text(input_data: Titles):
+async def process_title(input_data: Titles):
     # Extract the titles from the request
     received_titles = input_data.titles
+    predicted_labels = predcit_titles(received_titles)
     
-    max_len = 50  # Maximum number of words in a title (padding/truncating length)
+    return {"predicted_labels": predicted_labels} 
 
-    with open('tokenizer.json', 'r') as json_file:
-        tokenizer_json = json_file.read()
-
-    tokenizer = tokenizer_from_json(tokenizer_json)
-
-    # convert the titles to sequence
-    sequences = tokenizer.texts_to_sequences(received_titles)
-
-    # pad the sequences to the same length
-    padded_sequences = pad_sequences(sequences, maxlen=max_len, padding='post', truncating='post')
-
-    # perform prediction  
-    predicted_labels = supervised_model.predict(padded_sequences)
-    
-    # process -> 0 = "relevant", 1 = "irrelevant"
-    # processed_predicted_labels = ["relevant" if pred==0 else "irrelevant" for pred in predicted_labels ]
-
-    # Convert probabilities to binary predictions
-    threshold = 0.6
-    binary_predictions = (predicted_labels >= threshold).astype(int)
-    binary_predictions = binary_predictions.tolist()
-    
-    return {"predicted_labels": binary_predictions} 
-
-
-import json
 
 from unsupervised.process_data import DataProcessor
 dp = DataProcessor() 
---
+++ supervised/title_prediction.py
@@ -0,0 +1,35 @@
+import numpy as np
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import tensorflow as tf    
+
+# Load the supervised model (either .h5 or .keras)
+supervised_model = tf.keras.models.load_model('supervised/supervised_clickbait_detection_model_V2.h5')
+
+def predcit_titles(received_titles):   
+      
+    max_len = 50  # Maximum number of words in a title (padding/truncating length)
+
+    with open('supervised/tokenizer.json', 'r') as json_file:
+        tokenizer_json = json_file.read()
+
+    tokenizer = tokenizer_from_json(tokenizer_json)
+
+    # convert the titles to sequence
+    sequences = tokenizer.texts_to_sequences(received_titles)
+
+    # pad the sequences to the same length
+    padded_sequences = pad_sequences(sequences, maxlen=max_len, padding='post', truncating='post')
+
+    # perform prediction  
+    predicted_labels = supervised_model.predict(padded_sequences)
+    
+    # process -> 0 = "relevant", 1 = "irrelevant"
+    # processed_predicted_labels = ["relevant" if pred==0 else "irrelevant" for pred in predicted_labels ]
+
+    # Convert probabilities to binary predictions
+    threshold = 0.6
+    binary_predictions = (predicted_labels >= threshold).astype(int)
+    binary_predictions = binary_predictions.tolist()
+    
+    return binary_predictions
