13bb902978d0ed69a2e2d8ee6b576f67b19c14eb
Ranindu <raninduharischandra12@gmail.com>
2024-09-26 09:32:15 +0530

    fixed tokenizer problem

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from models import Titles
 import numpy as np
-from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import tensorflow as tf
 
@@ -10,7 +10,7 @@ router = APIRouter()
 
 
 # Load the supervised model (either .h5 or .keras)
-supervised_model = tf.keras.models.load_model('supervised_clickbait_detection_model.h5')
+supervised_model = tf.keras.models.load_model('supervised_clickbait_detection_model_V2.h5')
 
 # POST route to receive an array of titles
 @router.post("/single-title-detection")
@@ -18,12 +18,12 @@ async def process_text(input_data: Titles):
     # Extract the titles from the request
     received_titles = input_data.titles
     
-    vocab_size = 10000  # Maximum number of words in the vocabulary
     max_len = 50  # Maximum number of words in a title (padding/truncating length)
 
-    # Initialize and fit the tokenizer
-    tokenizer = Tokenizer(num_words=vocab_size, oov_token='<OOV>')
-    tokenizer.fit_on_texts(received_titles)
+    with open('tokenizer.json', 'r') as json_file:
+        tokenizer_json = json_file.read()
+
+    tokenizer = tokenizer_from_json(tokenizer_json)
 
     # convert the titles to sequence
     sequences = tokenizer.texts_to_sequences(received_titles)
@@ -36,8 +36,10 @@ async def process_text(input_data: Titles):
     
     # process -> 0 = "relevant", 1 = "irrelevant"
     # processed_predicted_labels = ["relevant" if pred==0 else "irrelevant" for pred in predicted_labels ]
+
     # Convert probabilities to binary predictions
-    threshold = 0.5
+    threshold = 0.6
     binary_predictions = (predicted_labels >= threshold).astype(int)
+    binary_predictions = binary_predictions.tolist()
     
     return {"predicted_labels": binary_predictions} 
